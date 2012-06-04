bbb4b74a01324dc97168f5aa2f7206415bacce19
CrBoy <crboy@crboy.net>
2012-06-04 11:53:18 +0800

    Revise the install script and ignore the linked file in git

--- install.bat
+++ install.bat
@@ -4,6 +4,9 @@ set HOME="%UserProfile%"
 set VIMFILES="%UserProfile%\vimfiles"
 set VIMRC="%UserProfile%\_vimrc"
 
+echo 建立 autoload 資料夾...
+mkdir "%VIMFILES%\autoload"
+
 echo 連結 pathogen...
 mklink "%VIMFILES%\autoload\pathogen.vim" "..\bundle\pathogen\autoload\pathogen.vim"
 
